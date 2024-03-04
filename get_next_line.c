e86f764d592ab62ce6e19aacc3c9fac3eb22ad3c
Sebastian Wong <sebwong@c1r8s8.42singapore.sg>
2024-03-04 16:17:53 +0800

    new

--- get_next_line.c
+++ get_next_line.c
@@ -28,10 +28,8 @@ until the null terminator.
 #include "get_next_line.h"
 
 
-char	*identify()
-
 char *ft_read(big)
-{
+{ 
 	//read file until reach a newline, then stop and call a function to process the string
 	int bytesRead;
 
@@ -41,18 +39,6 @@ char *ft_read(big)
 		ft_strchr(bytesRead, "\n");
 	}
 }
-
-//this function transfers everything before the newline
-//from the static variable to another variable
-char	*ft_pre_nl()
-
-//this function updates the static variable to everything
-//after the newline
-char	*ft_post_nl()
-
-//after that, join the new characters 
-
-
 
 char *get_next_line(int fd)
 {
@@ -64,32 +50,13 @@ char *get_next_line(int fd)
 		return (NULL);
 
 	//newline is identified and reading is stopped
-	ft_identifier(big, "\n");
 
 	//transfer the line chars into a new variable and remove them from the 
 	//static var and leave only the post-newline chars inside
-	ft_pre()
-	ft_post()
-
 	//return the line chars to the main func
 	return (line);
 }
-/*
-//In the test function, the file is opened and GNL is repeatedly called until the end of file 
-char  *get_next_line(int fd)
-{
-  int  bytes_read;
-  char *cup_buffer;
-  
-  cup_buffer = ft_calloc(3 + 1, sizeof(char));
-  if (!cup_buffer)
-    return (NULL);
-  bytes_read = read(fd, cup_buffer, 3);
-  if (bytes_read <= 0)
-    return (NULL);
-  return (cup_buffer);
-}
-*/
+
 int main()
 {
 	int count;

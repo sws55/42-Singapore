b1f183c6cda5d605808dc28c539574aa26da2125
Sebastian <chronisam@LAPTOP-N1BESTKP.localdomain>
2024-05-08 01:11:57 +0800

    GNL done?

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -11,14 +11,22 @@
 /* ************************************************************************** */
 
 #ifndef	GET_NEXT_LINE_H
-#define	GET_NEXT_LINE_H
+#   define	GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+#   define BUFFER_SIZE 10
+#endif
 
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-
-int	BUFFER_SIZE = 3;
+#include <stdlib.h>
 
 char	*get_next_line(int fd);
+char	*ft_calloc(size_t element, size_t size);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char *s);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
+char	*ft_strjoin(char *s1, char *s2);
 
 #endif
